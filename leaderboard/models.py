c27221b0d06e1eaeea456171df1dbd34f04a9145
guy-watson <95031920+guy-watson@users.noreply.github.com>
2023-03-20 17:26:16 +0000

    Delete leaderboard directory

--- leaderboard/models.py
+++
@@ -1,6 +0,0 @@
-# Aurthor: Kaloyan Gaydarov and Taariq Fadhill 
-from django.db import models
-
-class Score(models.Model):
-    player_name = models.CharField(max_length=100)
-    score = models.IntegerField()
--- leaderboard/urls.py
+++
@@ -1,8 +0,0 @@
-# Aurthor: Kaloyan Gaydarov and Taariq Fadhill 
-from django.urls import path
-from .views import leaderboard
-
-urlpatterns = [
-    path('', leaderboard, name='index'),
-    
-]
