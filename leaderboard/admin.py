904671c34588b958f1d3338f35698dcbb9bc5b43
guy-watson <95031920+guy-watson@users.noreply.github.com>
2023-03-20 17:31:14 +0000

    ++ leaderboard

---
+++ leaderboard/admin.py
@@ -0,0 +1,6 @@
+# Aurthor: Kaloyan Gaydarov and Taariq Fadhill 
+from django.contrib import admin
+
+from .models import Score
+
+admin.site.register(Score)
---
+++ leaderboard/apps.py
@@ -0,0 +1,6 @@
+# Aurthor: Kaloyan Gaydarov and Taariq Fadhill 
+from django.apps import AppConfig
+
+class LeaderboardConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "leaderboard"
