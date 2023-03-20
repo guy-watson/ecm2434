b6a26c8c8f3ea9a0a70f8d5472942042021c6dc6
guy-watson <95031920+guy-watson@users.noreply.github.com>
2023-03-20 17:27:06 +0000

    Delete teams directory

--- teams/urls.py
+++
@@ -1,8 +0,0 @@
-# Aurthor: Kaloyan Gaydarov and Taariq Fadhill 
-from django.urls import path
-from .views import add_member, team_detail
-
-app_name = 'teams'
-urlpatterns = [
-    path('add_member/', add_member, name='add_member'),
-]
